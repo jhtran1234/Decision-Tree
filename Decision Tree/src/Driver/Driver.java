52be8a388418931544053635ca0a88d3bc2a8857
Jeffrey Tran <jhtran1234@gmail.com>
2024-05-08 10:41:03 -0700

    Code cleanup

--- Decision Tree/src/Driver/Driver.java
+++ Decision Tree/src/Driver/Driver.java
@@ -18,26 +18,31 @@ import Data.DecisionTree;
  */
 public class Driver {
 	public static double entropy(int p, int n) {
-		double p1 = ((double) p)/((double) p + (double) n);
-		double p2 = ((double) n)/((double) p + (double) n);
+		final double p1 = ((double) p)/((double) p + (double) n);
+		final double p2 = ((double) n)/((double) p + (double) n);
 		return 0.0 - p1*Math.log(p1)/Math.log(2) - p2*Math.log(p2)/Math.log(2);
 	}
 	
 	public static void main(String[] args) throws Exception {
 		// read in CSV
-		Scanner scan = new Scanner(new BufferedReader(new FileReader("table1.csv")));
-		ArrayList<String> attributeNames = new ArrayList<String>(Arrays.asList(scan.nextLine().split(",")));
+		final Scanner scan = 
+				new Scanner(new BufferedReader(new FileReader("table1.csv")));
+		final ArrayList<String> attributeNames = 
+				new ArrayList<>(Arrays.asList(scan.nextLine().split(",")));
 		attributeNames.remove(attributeNames.size() - 1);
-		DataTable dataTable = new DataTable(attributeNames);
+		final DataTable dataTable = new DataTable(attributeNames);
 		
 		while(scan.hasNext()) {
-			ArrayList<String> row = new ArrayList<String>(Arrays.asList(scan.nextLine().split(",")));
-			boolean result = row.remove(row.size() - 1).equalsIgnoreCase("T") ? true : false;
-			DataRow r = new DataRow(attributeNames, row, result);
+			final ArrayList<String> row = 
+					new ArrayList<>(Arrays.asList(scan.nextLine().split(",")));
+			final boolean result = 
+					row.remove(row.size() - 1).equalsIgnoreCase("T") ? 
+							true : false;
+			final DataRow r = new DataRow(attributeNames, row, result);
 			dataTable.insertRow(r);
 		}
 		
-		DecisionTree tree = new DecisionTree(dataTable);
+		final DecisionTree tree = new DecisionTree(dataTable);
 		System.out.println();
 	}
 }

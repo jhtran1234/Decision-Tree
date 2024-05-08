abd3d59986c8a37a44463e74f8b50f2c732d016a
Jeffrey Tran <jhtran1234@gmail.com>
2024-05-08 10:44:49 -0700

    Code Cleanup

--- Decision Tree/src/Driver/Driver.java
+++ Decision Tree/src/Driver/Driver.java
@@ -1,6 +1,5 @@
 package Driver;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Arrays;
--- Decision Tree/src/Node/TreeNode.java
+++ Decision Tree/src/Node/TreeNode.java
@@ -20,7 +20,7 @@ public class TreeNode {
 		table = null;
 	}
 	
-	public TreeNode(String attribute, DataTable t) {
+	public TreeNode(final String attribute, final DataTable t) {
 		this.attribute = attribute;
 		children = new HashMap<>();
 		table = t.clone();
@@ -30,7 +30,7 @@ public class TreeNode {
 	 * @return HashSet<String> of all the attribute's options in the table
 	 */
 	public HashSet<String> findOptions() {
-		HashSet<String> options = new HashSet<String>();
+		final HashSet<String> options = new HashSet<>();
 
 		for(DataRow r : table.table) {
 			options.add(r.attributes.get(attribute));
@@ -40,18 +40,19 @@ public class TreeNode {
 	}
 
 	/**
-	 * Recursive method to constantly create new children until base Final nodes can be placed
+	 * Recursive method to constantly create new children until base Final
+	 * nodes can be placed
 	 */
 	public void makeChildren() {
-		HashSet<String> options = findOptions();
+		final HashSet<String> options = findOptions();
 
 		// for each option of the attribute, find next best entropy child and build
-		for(String option : options) {
-			DataTable t = table.singleOption(attribute, option);
+		for (String option : options) {
+			final DataTable t = table.singleOption(attribute, option);
 			t.removeAttribute(attribute);
 
 			// check to see if all true or all false			
-			if(t.table.size() <= 0) {
+			if (t.table.size() <= 0) {
 				System.out.println("Error! Table empty");
 				System.exit(0);
 			}
@@ -59,7 +60,8 @@ public class TreeNode {
 				children.put(option, new Final(t.table.get(0).result));
 			}
 			else if(t.listAttributeNames.size() > 0) {
-				TreeNode node = new TreeNode(t.chooseAttribute().get(0), t);
+				final TreeNode node = 
+						new TreeNode(t.chooseAttribute().get(0), t);
 				children.put(option, node);
 
 				node.makeChildren();
@@ -67,7 +69,7 @@ public class TreeNode {
 			else {
 				double trueCount = 0;
 				double falseCount = 0;
-				for(DataRow r : t.table) {
+				for (DataRow r : t.table) {
 					if(r.result) {
 						trueCount ++;
 					}
@@ -76,7 +78,12 @@ public class TreeNode {
 					}
 				}
 
-				TreeNode s = (trueCount >= falseCount) ? children.put(option, new Final(true, trueCount / (trueCount+falseCount))) : children.put(option, new Final(false, falseCount / (trueCount+falseCount)));
+				final TreeNode s = (trueCount >= falseCount) ?
+						children.put(option, new Final(true, trueCount /
+								(trueCount + falseCount))) :
+									children.put(option, 
+											new Final(false, falseCount / 
+													(trueCount + falseCount)));
 			}
 		}
 	}
